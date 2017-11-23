2a0ea13b3cfe9def1ef98d646fbcf244dc4f47aa
Filip Lindbladh <filiplindbladhh@gmail.com>
2017-11-23 12:38:11 +0100

    deleted some shit

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="hello">
+  <div>
+      <h1>Start</h1>
   
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Start',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -17,18 +18,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
