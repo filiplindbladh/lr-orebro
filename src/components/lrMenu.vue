0fac9ccc1a35608b3ae89b88990e68279c829d4c
Linda <li0000an@gmail.com>
2017-11-28 10:12:04 +0100

    new style on menu

--- src/components/lrMenu.vue
+++ src/components/lrMenu.vue
@@ -5,12 +5,12 @@
                 <img class="lrLogo" src="../img/logo.png">
             </div>
             <div class="menuItems">
-                <a href="#">START</a>
-                <a href="#">TJÄNSTER</a>
-                <a href="#">OM OSS</a>
-                <a href="#">KONTAKT</a>
-                <a class="button is-primary is-outlined has-text-weight-bold">LR-ONLINE</a>
-                <a class="button is-primary is-outlined"><img src="../img/lt-fortnox.png"></a>
+                <a class="menuItem" href="#">START</a>
+                <a class="menuItem" href="#">TJÄNSTER</a>
+                <a class="menuItem" href="#">OM OSS</a>
+                <a class="menuItem" href="#">KONTAKT</a>
+                <a class="button is-primary is-outlined lr-button-online">LR-ONLINE</a>
+                <a class="button is-primary is-outlined lr-button-fortnox"><img src="../img/lt-fortnox.png"></a>
             </div>
         </div>
     </div>
@@ -28,38 +28,53 @@ export default {
         flex-direction: row; 
         height: 75px;
         width: 100%;
+        background-color: #f5f5f5;
+    }
+    // Company logo
+    .lrLogoContainer {
+        display: flex;
+        align-items: center; 
     }
     .lrLogo {
-        height: 70px;
+        height: 65px;
         display: flex; 
         align-items: center;
     }
     .lrMenuContainer {
         display: flex;
     }
-    .lrLogoContainer {
-        display: flex; 
-    }
     .menuItems {
         display: flex;
-        justify-content: space-around;
+        justify-content: flex-end;
         flex-direction: row;
         width: 100%; 
         align-items: center;
-        a {
+        .menuItem {
+            padding: 0 20px;
+            font-weight: bold;
+            font-size: 0.85rem;
             color: $black;
             &:hover {
                 color: $blue;
             }
         }
-        img {
-            height: 17px;
-            width: 91px;
-            &:hover {
-                color:white; 
-            }
+        .lr-button-fortnox {
+            margin-left:20px;
+            width: 100.43px;
+            height: 30.59px;
         }
-      
-    }
-    
+        .lr-button-online {
+            font-size: 0.85rem;
+            font-weight: bold;
+            margin: 0 20px;
+            width: 100.43px;
+        }
+        img {
+            height: 15px;
+            // &:hover {
+            // -webkit-filter: invert(1);
+            // filter: invert(1);
+            // }
+        }  
+    }    
 </style>
