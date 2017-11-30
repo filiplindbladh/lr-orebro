e86d31b3ea7de144db62c585870ef73372f3f5dc
Linda <li0000an@gmail.com>
2017-11-30 15:08:33 +0100

    Added a class to p on lrServices and added some content in lrStaff

--- src/components/lrStaff.vue
+++ src/components/lrStaff.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-
-    </div>
+  <div class="section container">
+    <h2>Mer än bara siffror</h2>
+    <hr class="lrHeaderLine">
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis quas quibusdam harum quam. Fuga at saepe veritatis voluptates? Neque consequuntur distinctio voluptatem consequatur minima ab accusamus repellendus quidem laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis quas quibusdam harum quam. Fuga at saepe veritatis voluptates? Neque consequuntur Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis quas quibusdam harum quam. Fuga at saepe veritatis voluptates? Neque consequuntur distinctio voluptatem consequatur minima ab accusamus repellendus quidem laboriosam. distinctio voluptatem consequatur minima ab accusamus repellendus quidem laboriosam.</p>
+  </div>
 </template>
 
 <script>
@@ -12,4 +14,13 @@ export default {
 
 <style lang="scss">
 @import '../style/_variables.scss';
+  .lrHeaderLine {
+        background-color: $red;
+        height:5px; 
+    }
+  .section {
+    display: flex;
+    align-items: center; 
+    flex-direction: column; 
+  }
 </style>
