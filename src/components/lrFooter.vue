0287fb7bc101c4926f6e72d859b807bc07b908d8
Linda <li0000an@gmail.com>
2017-11-30 14:10:00 +0100

    Added template for all components

---
+++ src/components/lrFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'lrFooter'
+}
+</script>
+
+<style lang="scss">
+@import '../style/_variables.scss';
+</style>
