b0ee3850e127b413cf5c4cecd0d17b8f82164cc1
Linda <li0000an@gmail.com>
2017-12-01 15:08:28 +0100

    added size on h2 h3 on smaller screens. added hover on staff picture

--- src/components/lrStaff.vue
+++ src/components/lrStaff.vue
@@ -5,66 +5,45 @@
 
     <p class="mainText">I samband med rådgivningen möter du ofta fler medarbetare hos oss, t.ex redovisningskonsulter, företagsrådvigare, skatteexperter. Vi analyserar inte bara det föregående året, utan kan även hjälpa di gmed att se framåt och göra analyser och antaganden.</p>
 
-    <div class="columns is-mobile is-multiline">
+    <!-- pictures of staff -->
+    <div class="columns is-mobile is-multiline lrPictures">
       <div class="column is-half-mobile  is-one-quarter-tablet"> 
         <a href="#">
             <img src="http://via.placeholder.com/250x250" alt="">
         </a> 
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-              <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-              <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-              <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-              <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-              <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-              <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-              <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-              <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-            <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-            <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+          <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
       <div class="column is-half-mobile  is-one-quarter-tablet">
-          <a href="#">
-            <img src="http://via.placeholder.com/250x250" alt="">
-          </a> 
+        <img class="lrStaffMember" src="http://via.placeholder.com/250x250" alt="">
       </div>
     </div>
   </div>
@@ -87,10 +66,19 @@ export default {
     align-items: center; 
     flex-direction: column; 
   }
-
   .lrStaff p {
     text-align: center;
     width: 70%;
     margin-bottom: 30px;
+  }
+  .lrPictures {
+    text-align: center; 
+  }
+  .lrStaffMember {
+    width: 250px;
+    height: auto;
+    &:hover {
+      filter: blur(2px) grayscale(100%);
+    }
   }
 </style>
