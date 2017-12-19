9219f478cf901a98167a509f36a7148715c86d75
Filip Lindbladh <filiplindbladhh@gmail.com>
2017-12-19 10:14:53 +0100

    added slider header, changed id on facebookFeed

--- src/components/lrHeader.vue
+++ src/components/lrHeader.vue
@@ -1,17 +1,64 @@
 <template>
-    <div class="section header overlay">
-        <div class="lrHeader container">
-            <h1> VÄLKOMMEN TILL <br /> DIN FRAMTIDSBYRÅ </h1>
-            <hr class="lrHeaderLineHeader">
-            <a v-scroll-to="'#sectionServices'" id="lr-button-more" class="button is-info is-outlined">LÄS MER</a>
-        </div>
-    </div>
+      <!-- swiper -->
+      <swiper :options="swiperOption">
+        <swiper-slide>
+            <div class="section header overlay">
+                <div class="lrHeader container">
+                    <h1> VÄLKOMMEN TILL <br /> DIN FRAMTIDSBYRÅ </h1>
+                    <hr class="lrHeaderLineHeader">
+                    <a v-scroll-to="'#sectionServices'" id="lr-button-more" class="button is-info is-outlined">LÄS MER</a>
+                </div>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="section header overlay">
+                <div class="lrHeader container">
+                    <h2>REGISTRERAT VERKLIG HUVUDMAN <br> HOS BOLAGSVERKET?</h2>
+                    <h4>– Sista dagen 30 Januari.</h4>
+                    <hr class="lrHeaderLineHeader">
+                    <a v-scroll-to="'#lrFacebookFeed'" id="lr-button-more" class="button is-info is-outlined">LÄS MER</a>
+                </div>
+            </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="section header overlay">
+              <div class="lrHeader container">
+                  <h2>HÅLL KOLL PÅ DIN<br> EKONOMI MED LR-ONLINE</h2>
+                  <hr class="lrHeaderLineHeader">
+                  <a href="/online" id="lr-button-more" class="button is-info is-outlined">LÄS MER</a>
+              </div>
+          </div>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+      </swiper>
 </template>
 
 <script>
-    export default {
-      name: 'lrHeader'
+export default {
+  name: 'lrHeader',
+  data () {
+    return {
+      swiperOption: {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 8000,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        navigation: {
+          //nextEl: '.swiper-button-next',
+          //prevEl: '.swiper-button-prev'
+        }
+      }
     }
+  }
+}
 </script>
     
 <style lang="scss">
@@ -60,7 +107,7 @@
         -webkit-box-direction: normal;
             -ms-flex-direction: column;
                 flex-direction: column;
-        h1 {
+        h1,h2,h3,h4 {
             text-align: center;
             color: $white;
             z-index: 100;
