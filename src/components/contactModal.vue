eb64e3afe7b15e4662ad413346ae5984f59e8fd3
Filip Lindbladh <filiplindbladhh@gmail.com>
2018-01-08 19:29:58 +0100

    Added working formspree form!!!!

--- src/components/contactModal.vue
+++ src/components/contactModal.vue
@@ -11,7 +11,7 @@
                 <h1>Hej</h1>
                 <img src="../img/waving-hand.png">
                 <p>Vad kan vi hjälpa dig med? Skriv ett meddelande så kontaktar vi dig. Önskas en offert vänligen bifoga antal anställda samt fakturor per månad.</p>
-                <form action="">
+                <form id="gform" form="form" method="POST" action="http://formspree.io/hej@lr-orebro.se">
                     <div class="control has-icons-left has-icons-right">
                         <p :class="{ 'control': true }">
                             <input v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }" name="email" type="text" placeholder="Email">
@@ -28,24 +28,24 @@
                     <div class="field is-grouped is-expanded">
                         <div class="control">
                             <label class="label">Årsomsättning (mKr):</label>
-                            <input class="input valueOne" type="number" v-model="valueOne">
+                            <input class="input valueOne" type="number" name="Omsattning" v-model="valueOne">
                         </div>
                         <div class="control">
                             <label class="label">Anställda:</label>
-                            <input class="input valueTwo" type="number" v-model="valueTwo">
+                            <input class="input valueTwo" type="number" name="Anstallda" v-model="valueTwo">
                         </div>
                         <div class="control">
                             <label class="label">Organisationsnummer:</label>
-                            <input class="input valueTwo" type="number">
+                            <input class="input valueTwo" name="Organisationsnummer" type="number">
                         </div>
                     </div>
                     </div>
-                    <textarea class="textarea" placeholder="Meddelande">
+                    <textarea class="textarea" name="Meddelande" placeholder="Meddelande">
                     </textarea>
                 </form>
                 </section>
                 <footer class="modal-card-foot">
-                <a class="button is-primary is-outlined">Skicka</a>
+                <button form="gform" value="send" type="submit" class="button is-primary is-outlined">Skicka</button>
                 <button class="button is-danger is-outlined" @click='close'>Cancel</button>
                 </footer>
             </div>
